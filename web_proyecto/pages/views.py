db846c2ece4ae4a42f95aec00f3f0efbae07ad53
unknown <80284825+JohnCorn27@users.noreply.github.com>
2021-10-19 20:12:36 -0500

    Proyecto Decoracion con usuario

--- web_proyecto/pages/views.py
+++ web_proyecto/pages/views.py
@@ -4,6 +4,13 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from .models import Page, Categoria, Video
 from .forms import PageForm, PageFormCategoria, PageFormVideo
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
+
+class StaffRequiredMixin(object):
+    @method_decorator(staff_member_required)
+    def dispatch(self,request, *args, **kwargs):
+        return super(StaffRequiredMixin, self).dispatch(request,*args,**kwargs)
 
 # Create your views here.
 class PageListView(ListView):
@@ -12,21 +19,25 @@ class PageListView(ListView):
 class PageDetalView(DetailView):
     model = Page
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageCreateVideo(CreateView):
     model = Video
     form_class = PageFormVideo
     success_url = reverse_lazy('pages:pages')
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageCreateCategoria(CreateView):
     model = Categoria
     form_class = PageFormCategoria
     success_url = reverse_lazy('pages:pages')
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageCreate(CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageUpdate(UpdateView):
     model = Page
     form_class = PageForm
@@ -35,6 +46,7 @@ class PageUpdate(UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageDelete(DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
